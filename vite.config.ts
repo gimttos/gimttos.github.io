434eb13e890055f1507c9c02ab0b5c34bcacf763
gimttos <gimttos@gmail.com>
2026-01-14 20:53:18 +0900

    deploy: cleanup git tracking and update constants

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './', // ⭐ Pages 필수
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  }
 })
